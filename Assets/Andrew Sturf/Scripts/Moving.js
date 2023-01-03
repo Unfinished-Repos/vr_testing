7932d7b26cd0b707d03153f54b5b1e75523bad00
Andrew Friedman <andrewfriedman01@gmail.com>
2023-01-02 23:18:44 -0500

    Add 2018 content

---
+++ Assets/Andrew Sturf/Scripts/Moving.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+function Start () {
+	
+	while (1 > 0) {
+		GetComponent.<Rigidbody>().velocity.x = Random.Range(1,3) * Time.deltaTime;
+		yield WaitForSeconds (Random.Range(1,5));
+	}
+	
+	while (1 > 0) {
+		GetComponent.<Rigidbody>().velocity.z = Random.Range(1,3) * Time.deltaTime;
+		yield WaitForSeconds (Random.Range(1,5));
+	}
+	
+	while (1 > 0) {
+		var random = Random.Range(1,3);
+		if (random == 1) {
+			GetComponent.<Rigidbody>().velocity.y = 8;
+		}
+		yield WaitForSeconds (Random.Range(1,3));
+	}
+}
